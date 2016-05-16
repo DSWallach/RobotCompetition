0ebff279dd08a4851a693682e728759ba7a00e20
David Wallach <dawallach@vassar.edu>
2016-05-16 18:09:54 -0400

    Commented and cleaned up code.

--- Ghost/Ghost.c
+++ Ghost/Ghost.c
@@ -1,17 +1,18 @@
-void forward(){//int left, int right){
-	/*int charge = get_create_battery_voltage ();
-	printf ("current charge: %d", charge);*/
+// Makes the robot move forward.
+void forward(){
 	motor(0,100);
 	motor(2,100);
 	msleep(25);
 }
 
+// Makes the robot back up.
 void backUp(){
 	motor(0,-100);
 	motor(2,-100);
 	msleep(25);
 }
 
+// Makes the robot move forward a large amount in one go.
 void pullUp(){
 	int i;
 	for(i = 0; i < 20; i++){
@@ -19,19 +20,21 @@ void pullUp(){
 	}
 }
 
+// Function for making the robot turn slightly left.
 void left(){
 	motor(0,-100);
 	motor(2,100);
 	msleep(50);
 }
 
+// Function for making the robot turn slightly right
 void right(){
 	motor(0,100);
 	motor(2,-100);
 	msleep(50);
 }
 
-// This turns the robot to the left
+// Turns the robot left 90 degrees.
 void turnLeft(){
 	int i;
 	for(i = 0; i < 10; i++){
@@ -39,7 +42,7 @@ void turnLeft(){
 	}
 }
 
-// This turns the robot to the right
+// Turns the robot right 90 degrees.
 void turnRight(){
 	int i;
 	for(i = 0; i < 10; i++){
@@ -47,7 +50,7 @@ void turnRight(){
 	}
 }
 
-// This turns the robot around to the right 180 degrees
+// Turns the robot right 180 degrees.
 void turnAround(){
 	int i; 
 	for(i = 0; i < 20; i++){
@@ -55,14 +58,7 @@ void turnAround(){
 	}
 }
 
-void stop(){
-	motor(0,-100);
-	motor(2,-100);
-	msleep(10);
-}
-
-
-//Detects an object in front and returns 1 for true and 0 for false
+// Detects an object in front and returns 1 for true and 0 for false
 int wallFront(){
 	if(analog_et(0) > 300){
 		return 1;
@@ -72,7 +68,7 @@ int wallFront(){
 	}
 }
 
-//Detects an object to the left and returns 1 for true and 0 for false
+// Detects an object to the left and returns 1 for true and 0 for false
 int wallLeft(){
 	if(analog_et(1) > 250){
 		return 1;
@@ -82,7 +78,7 @@ int wallLeft(){
 	}
 }
 
-//Detects an object to the right and returns 1 for true and 0 for false
+// Detects an object to the right and returns 1 for true and 0 for false
 int wallRight(){
 	if(analog_et(2) > 250){
 		return 1;
@@ -92,32 +88,38 @@ int wallRight(){
 	}
 }
 
-int checkExit(){
+// Checks the camera for the PacMan.
+int checkPacMan(){
+	
+	// Update the camera
 	camera_update();
-	// Color config for recognizing the shiny green ball
-	// Hue: 122 to 172
-	// Saturation: 134 to 255
-	// Value: 50 to 255
-	// printf("checkExit");
+	
+	// If there is at least one object on color_channel 0.
 	if (get_object_count(0) > 0){
+		// Get the objects height and width.
 		int h = get_object_bbox(0,0).height;
 		int w = get_object_bbox(0,0).width;
+		
+		// Print out the number of objects and the height and width.
+		// Useful for troubleshooting.
 		printf("Number of Objects: %d\n Biggest object height: %d, width: %d\n", get_object_count(1),h,w);
+		
+		// If the height/width is greater that 30/20, return 1 (PacMan found).
 		if ((h >= 30 && w >= 20) || (h >= 20 && w >= 30))
-		{
-			//pullUp();
-			return 1;
-		} 
+		return 1;
+		// If height/width is greater than 20/10, move forward then return 1 (PacMan found).
 		else if ((h >= 20 && w >= 10) || (h >= 10 && w >= 20))
 		{
 			pullUp();
 			printf ("Heaading to target\n");
 			return 1;
 		}
+		// Otherwise return 0 (No PacMan).
 		else {
 			return 0;
 		}
 	} 
+	// Otherwise return 0 (No PacMan).
 	else {
 		return 0;
 	}
@@ -129,136 +131,107 @@ int *solveMaze(int lastTurnDir[]){
 	// 1 = East
 	// 2 = South
 	// 3 = West
-	int tempArray[2];
-	int *turnDir = &tempArray[0];
-	turnDir[0] = lastTurnDir[0];
-	turnDir[1] = lastTurnDir[1];
+	int tempArray[2];				// Used to store lastTurn and current direction.
+	int *turnDir = &tempArray[0];	// Used to point to tempArray.
+	turnDir[0] = lastTurnDir[0];	// Assign the first value to the first value of input array lastTurnDir.
+	turnDir[1] = lastTurnDir[1];	// Same as above but for the second value.
+	
+	// NOTE: All printf statements are included for troubleshooting purposes.
+	// If either of the two sensors that make up the front bumper are hit ...
 	if (digital(13) == 1 || digital (12) == 1){
-		printf("Front Bumper\n");
+		printf("Front Bumper\n");	
+		backUp();					// Back up three times.
+		backUp();					
 		backUp();
-		backUp();
-		backUp();
-		turnRight();
-		return turnDir;
+		turnRight();				// Turn right 90 degrees.
+		return turnDir;				// Return turnDir unchanged.
 	}
+	// If the left bumper is hit ...
 	else if(digital(14) == 0){
-		printf("Left Bumper\n");
-		backUp();
-		backUp();
-		right();
+		printf("Left Bumper\n");	
+		backUp();					// Back up and turn slightly right. 
 		backUp();
 		right();
+		backUp();
 		right();
-		return turnDir;
+		right();
+		return turnDir;				// Return turnDir unchanged.
 	}
 	else if(digital(15) == 0){
-		printf("Right Bumper\n");
-		backUp();
-		backUp();
-		left();
+		printf("Right Bumper\n");	
+		backUp();					// Back up and turn slightly left.
 		backUp();
 		left();
+		backUp();
 		left();
-		return turnDir;
+		left();
+		return turnDir;				// Return turnDir unchanged.
 	}
+	// If the IR detects no wall in front...
 	else if(wallFront() == 0){
+		
+		// If the IRs detect no walls on either side...
 		if (wallRight() == 0 && wallLeft() == 0){
-			printf("T intersection \n"); 
-			if(analog_et(3) < 100){
-				printf("PullUp\n");
-				pullUp();
-				turnDir[0] = 2;
-				return turnDir;
-			}
-			else if (analog_et(4) < 100 && turnDir[0] != 0){
-				printf("TurnRight\n");
-				turnRight();
-				turnDir[0] = 0;
-				turnDir[1] += 1;
-				return turnDir;
-			}
-			else if (analog_et(6) < 100 && turnDir[0] != 1){
-				printf("TurnLeft\n");
-				turnLeft();
-				turnDir[0] = 1;
-				turnDir[1] -= 1;
-				return turnDir;
-			}
-			else{	
-				pullUp();
-				turnDir[0] = 2;
-				return turnDir;
-			}
+			printf("T intersection \n"); 	
+			printf("PullUp\n");		
+			pullUp();				// Move forward 
+			turnDir[0] = 2;			// Update turnDir 
+			return turnDir;			
 		}
-		else if (checkExit())
+		
+		// Check for PacMan
+		else if (checkPacMan())
 		{
-			turnDir [0] = 5;
-			return turnDir;		
+			turnDir [0] = 5; 		// Update turnDir to signal PacMan found.
+			return turnDir;			
 		}
+		
+		// Else if there is no wall on the right...
 		else if(wallRight() == 0){
 			printf(" L intersection \n");
-			if(analog_et(3) < 100){
-				printf("PullUp\n");
-				pullUp();
-				turnDir[0] = 2;
-				return turnDir;
-			}
-			else if (analog_et(4) < 100 && turnDir[0] != 0){
-				printf("TurnRight\n");
-				turnRight();
-				turnDir[0] = 0;
-				turnDir[1] += 1;
-				return turnDir;
-			}
-			else{
-				printf("PullUp\n");
-				pullUp();
-				turnDir[0] = 2;
-				return turnDir;
-			}
-		}
-		else if(wallLeft() == 0){
-			printf("_| intersection \n");
-			if(analog_et(3) < 100){
-				printf("PullUp\n");
-				pullUp();
-				return turnDir;
-			}
-			else if (analog_et(6) < 100 && turnDir[0] != 1){
-				printf("TurnLeft\n");
-				turnLeft();
-				turnDir [0] = 1;
-				turnDir [1] -= 1;
-				return turnDir;
-			}
-			else{
-				printf("PullUp\n");
-				pullUp();
-				turnDir [0] = 2;
-				return turnDir;
-			}
-		} 
-		else {
-			forward();
+			printf("PullUp\n");
+			pullUp();				// Move forward
+			turnDir[0] = 2;			// Update turnDir
 			return turnDir;
 		}
+		
+		// Else if there is no wall on the left...
+		else if(wallLeft() == 0){
+			printf("_| intersection \n");
+			printf("PullUp\n");
+			pullUp();				// Move Forward
+			turnDir [0] = 2;		// Update turnDir
+			return turnDir;
+		} 
+		else {
+			forward();				// Move slightly forward
+			return turnDir;			// Return turnDir unchanged
+		}
 	}
+	
+	// If there is no wall on the left or right...
 	else if(wallRight() == 0 && wallLeft() == 0){
 		printf(" -- intersection \n");
-		if(analog_et(4) < 100 && turnDir[0] != 0){
+		
+		// If lastTurn is not 0 (right), turn right and update turnDir.
+		if(turnDir[0] != 0){
 			printf("TurnRight\n");
 			turnRight();
 			turnDir [0] = 0;
 			turnDir [1] += 1;
 			return turnDir;
 		}
-		else if (analog_et(6) < 100 && turnDir[0] != 1){
+		
+		// Else if lastTurn is not 1 (left), turn left and update turnDir
+		else if (turnDir[0] != 1){
 			printf("TurnLeft\n");
 			turnLeft();
 			turnDir [0] = 1;
 			turnDir [1] -= 1;
 			return turnDir;
 		}
+		
+		// Otherwise turn right and update turnDir
 		else{
 			printf("TurnRight\n");
 			turnRight();
@@ -267,6 +240,9 @@ int *solveMaze(int lastTurnDir[]){
 			return turnDir;
 		}
 	}
+	
+	// Else if there is no wall on the right and lastTurn is not 0 (right)
+	// Turn right and update turnDir.
 	else if (wallRight() == 0 && turnDir[0] != 0){
 		printf("Right intersection \n");
 		turnRight();
@@ -274,6 +250,8 @@ int *solveMaze(int lastTurnDir[]){
 		turnDir[1] += 1;
 		return turnDir;
 	}
+	// Else if there is no wall on the left and lastTurn is not 1 (left)
+	// Turn left and update turnDir.
 	else if (wallLeft() == 0 && turnDir[0] != 1){
 		printf(" Left intersection \n");
 		turnLeft();
@@ -281,6 +259,8 @@ int *solveMaze(int lastTurnDir[]){
 		turnDir [1] -= 1;
 		return turnDir;
 	}
+	
+	// Else if the IRs detect walls on all sides, turn 180 degrees and update turnDir
 	else if (wallFront() == 1 && wallRight() == 1 && wallLeft() == 1){
 		printf("Dead End \n");
 		turnAround();
@@ -291,32 +271,39 @@ int *solveMaze(int lastTurnDir[]){
 		turnDir [1] -= 2;
 		return turnDir;
 	}
-	forward ();
-	return turnDir;
+	
+	// Otherwise move slightly forward and return turnDir unchanged.
+	else {
+		forward ();
+		return turnDir;
+	}
 }
 
 int main(){
-	int i, 
-	j,
-	turnArray [2],
-	*tempP;
-	int pastDirArray [5] = {-1,-1,-1,-1,-1};
-	turnArray[0] = 2;
-	turnArray[1] = 0;
-	int h = 0;
-	int w = 0;
+	int i,										// Used in for loop. 
+	j, 											// Used in different for loop.
+	turnArray [2], 								// Stores lastTurn, and current direction information.
+	*tempP;										// Used to point to turnArray.
+	int pastDirArray [5] = {-1,-1,-1,-1,-1};	// Initialize list of past directions. NOTE: This ended up not being used for anything.
+	turnArray[0] = 2;							// Initialize lastTurn to 2 (forward).
+	turnArray[1] = 0;							// Initialize current direction to 0 (North).
+	
+	// Setup the camera.
 	camera_open();
-	while(1){
-		camera_update();
-		if (get_object_count(0) > 0){
-		h = get_object_bbox(0,0).height;
-		w = get_object_bbox(0,0).width;
-		}
+	
+	// Loop until "break" is called.
+	while(1){					
+		
+		// Run solveMaze three times.
 		for (i = 0; i < 3; i++){
+			
+			// Update pointer to turnArray.
 			tempP = solveMaze(turnArray);
 			
+			// Update lastTurn
 			turnArray [0] = tempP [0];
 			
+			// Update current direction.
 			if (tempP [1] == 5)
 			turnArray [1] = 0;
 			else if (tempP [1] == -1)
@@ -324,27 +311,32 @@ int main(){
 			else
 			turnArray [1] = tempP [1];
 			
-			for (j = 4; j > -1; j--)
-			pastDirArray[j] = pastDirArray[j - 1];
-			pastDirArray [0] = turnArray [1];
+			// Update pastDirArray
+			for (j = 4; j > -1; j--) {
+				pastDirArray[j] = pastDirArray[j - 1];
+				pastDirArray [0] = turnArray [1];
+			}
 			
+			// If PacMan was found freeze the motors and break out of the loop.
 			if (turnArray[0] == 5)
 			printf ("PacMan Found!\n Firing Weapons Array\n PacMan Neutralized!");
 			freeze (0);
 			freeze (2);
 			break;
 		}
-		if (turnArray[0] == 5)
-		break;
-		forward ();
 		
-		if(checkExit()){
+		// Check for PacMan, if found freeze the motors and break out of the loop.
+		if(checkPacMan()){
 			printf ("PacMan Found!\n Firing Weapons Array\n PacMan Neutralized!");
 			freeze (0);
 			freeze (2);
 			break;
 		}
 	}
+	
+	// Shutdown the Camera.
 	camera_close();
+	
+	// Exit.
 	return 0;
 }
--- PacMan/PacMan.c
+++ PacMan/PacMan.c
@@ -1,18 +1,22 @@
-int counter = 2;
-int trap = 0;
+// Global Variables
+int counter = 2; 	// A counter tracking the number of turns made since the PacMan was trapped.
+int trap = 0;		// 1 if the PacMan is trapped, 0 otherwise
 
+// Makes the robot move forward.
 void forward(){
 	motor(0,95);
 	motor(2,100);
 	msleep(25);
 }
 
+// Makes the robot back up.
 void backUp(){
 	motor(0,-100);
 	motor(2,-100);
 	msleep(25);
 }
 
+// Makes the robot move forward a lot.
 void pullUp(){
 	int i;
 	for(i = 0; i < 20; i++){
@@ -20,12 +24,14 @@ void pullUp(){
 	}
 }
 
+// Makes the robot turn slightly left.
 void left(){
 	motor(0,-100);
 	motor(2,100);
 	msleep(50);
 }
 
+// Makes the robot turn slightly right.
 void right(){
 	motor(0,100);
 	motor(2,-100);
@@ -33,7 +39,7 @@ void right(){
 }
 
 
-// This turns the robot to the left
+// This turns the robot to the left 90 degrees, and increments counter.
 void turnLeft(){
 	int i;
 	counter++;
@@ -45,19 +51,19 @@ void turnLeft(){
 	ao();
 }
 
-// This turns the robot to the right
+// This turns the robot to the right 90 degrees, and increments counter.
 void turnRight(){
 	int i;
 	counter++;
 	printf("counter = %d \n", counter);
 	for(i = 0; i < 8; i++){
 		right();
-		}
+	}
 	pullUp();
 	ao();
 }
 
-// This turns the robot around to the right 180 degrees
+// This turns the robot to the right 180 degrees
 void turnAround(){
 	int i; 
 	for(i = 0; i < 17; i++){
@@ -66,15 +72,7 @@ void turnAround(){
 	ao();
 }
 
-void stop(){
-	motor(0,-100);
-	msleep(10);
-	ao();
-	motor(2,-100);
-	msleep(10);
-	ao();
-}
-
+// Reset trap and counter.ao()
 void reset(){
 	if (trap == 1 && counter > 2){
 		printf("reset \n");
@@ -118,218 +116,283 @@ int wallRight(){
 	}
 }
 
-int goal(){
-	return 0;
-}
-
+// This function performs the maze solving algorithm.
+// NOTE: All print statements are for troubleshooting purposes.
 int solveMaze(int lastTurn){
-	if (digital(13) == 1){
+	
+	// If either sensor in the front bumper is hit, back up and turn left 90 degrees.
+	if (digital(13) == 1 || digital(12) == 1){
 		printf("Front Bumper\n");
 		backUp();
 		backUp();
 		turnLeft();
 		return lastTurn;
 	}
+	// Else if the left bumper is hit, back up and turn slightly right.
 	else if(digital(14) == 0){
 		printf("Left Bumper\n");
-		int i;
 		backUp();
 		right();
 		return lastTurn;
 	}
+	
+	// Else if the right bumper is hit, back up and turn slightly left.
 	else if(digital(15) == 1){
 		printf("Right Bumper\n");
-		int i;
 		backUp();
 		left();
 		return lastTurn;
 	}
+	
+	// Else if there is no wall in front...
 	else if(wallFront() == 0){
+		
+		// If there is no wall on either side...
 		if (wallRight() == 0 && wallLeft() == 0){
 			printf("T intersection \n"); 
-			stop();
+			
+			// If the IR beacon detects the other beacon straight ahead, move forward, return 2 (forward).
 			if(analog_et(3) < 100 ){
 				printf("PullUp\n");
 				pullUp();
 				pullUp();
 				return 2;
 			}
+			
+			// Else if the IR beacon detects the other beacon to the right,
+			// and the last turn was not right, and the robot is not trapped,
+			// turn right, return 0 (right).
 			else if (analog_et(4) < 100 && lastTurn != 0 && trap != 1){
 				printf("TurnRight\n");
 				turnRight();
 				return 0;
 			}
+			
+			// Else if the IR beacon detects the other beacon to the left,
+			// and the last turn was not left, and the robot is trapped,
+			// turn left, return 1 (left).
 			else if (analog_et(6) < 100 && lastTurn != 1 && trap != 0){
 				printf("TurnLeft\n");
 				turnLeft();
 				return 1;
 			}
+			
+			// Else if the robot is trapped and the lastTurn was not right, turn right.
 			else if (trap == 1 && lastTurn != 1){
 				printf("Trapped Turnright\n");
 				turnRight();
 				return 0;
 			}
+			
+			// Otherwise go foward.
 			else{	
 				pullUp();
 				return 2;
 			}
 		}
+		
+		// Else if there is no wall on the right...
 		else if(wallRight() == 0){
 			printf(" L intersection \n");
-			stop();
+			
+			// If the IR beacon detects the other beacon sraight ahead,
+			// and the robot is not trapped, go forward.
 			if(analog_et(3) < 100 && trap != 1){
 				printf("PullUp\n");
 				pullUp();
 				return 2;
 			}
+			
+			// Else if the IR beacon detects the other beacon to the right,
+			// and the last turn was not right, and the robot is not trapped,
+			// turn right, return 0 (right).
 			else if (analog_et(4) < 100 && lastTurn != 0 && trap != 1){
 				printf("TurnRight\n");
 				turnRight();
 				return 0;
 			}
+			
+			// Else if the robot is trapped, turn right.
 			else if (trap == 1){
 				printf("Trapped TurnRight\n");
 				turnRight();
 				return 0;
 			}
+			
+			// Otherwise pull up.
 			else{
 				printf("PullUp\n");
 				pullUp();
 				return 2;
 			}
 		}
+		
+		// Else if there is no wall on the left...
 		else if(wallLeft() == 0){
-			stop();
 			printf("_| intersection \n");
+			
+			// If the IR beacon detects the other beacon sraight ahead,
+			// and the robot is not trapped, go forward.
 			if(analog_et(3) < 100 && trap != 1){
 				printf("PullUp\n");
 				pullUp();
 				return lastTurn;
 			}
+			
+			// Else if the IR beacon detects the other beacon to the left,
+			// and the robot is trapped, turn left.
 			else if (analog_et(6) < 100 || trap == 1){ 
 				printf("TurnLeft\n");
 				turnLeft();
 				return 1;
 			}
-			/*else if (trap == 1){
-				printf("Trapped PullUpt\n");
-				pullUp();
-				return 1;
-			}*/
+			
+			// Otherwise go forward.
 			else{
 				printf("PullUp\n");
 				pullUp();
 				return 2;
 			}
 		} 
+		
+		// Otherwise go slightly forward.
 		else {
 			forward();
 			return lastTurn;
 		}
 	}
+	
+	// Else if there is no wall on the left or the right. 
 	else if(wallRight() == 0 && wallLeft() == 0){
-		stop();
 		printf(" -- intersection \n");
+		
+		// Else if the IR beacon detects the other beacon to the right,
+		// and the last turn was not right, and the robot is not trapped,
+		// turn right, return 0 (right).
 		if(analog_et(4) < 100 && lastTurn != 0 && trap != 0){
 			printf("TurnRight\n");
 			turnRight();
 			return 0;
 		}
-		else if (analog_et(6) < 100 && lastTurn != 1 || trap == 1){
+		
+		// Else if the IR beacon detects the other beacon to the left,
+		// and the last turn was not left, or the robot is trapped,
+		// turn left, return 1 (left).
+		else if ((analog_et(6) < 100 && lastTurn != 1) || trap == 1){
 			printf("TurnLeft\n");
 			turnLeft();
 			return 1;
 		}
-		/*else if (trap = 1){
-			printf("turnRight\n");
-			turnRight();
-			counter++
-			return 0;*/
+		
+		// Otherwise turn right.
 		else{
 			printf("TurnRight\n");
 			turnRight();
 			return 0;
 		}
 	}
+	
+	// Else if there is no wall on the right, and the last turn was not right, turn right. 
 	else if (wallRight() == 0 && lastTurn != 0){
-		stop();
 		printf("Right intersection \n");
 		turnRight();
 		return 0;
 	}
+	
+	// Else if there is no wall on the left. and the last turn was not left, turn left.
 	else if (wallLeft() == 0 && lastTurn != 1){
-		stop();
 		printf(" Left intersection \n");
 		turnLeft();
 		return 1;
 	}
+	
+	// Else if there is a wall in front and on both sides...
 	else if (wallFront() == 1 && wallRight() == 1 && wallLeft() == 1){
 		printf("Dead End \n");
+		
+		// If the IR beacon detects the other beacon straight ahead.
 		if (analog_et(3) < 100){
 			trap = 1;
 			counter = 0;
 			printf("Trapped \n");
 		}
+		
+		// Turn around.
 		turnAround();
 		return 4;
 	}
+	
+	// Otherwise go forward
+	else
+		forward();
+		return lastTurn;
 }
 
+// Color config
+// Hue: 44 to 64
+// Saturation: 40 to 255
+// Value: 150 to 255
 
+// Function for checking for the maze exit. 
 int checkExit(){
-	//camera_open();
+	
+	// Update the camera.
 	camera_update();
-	// Color config
-	// Hue: 44 to 64
-	// Saturation: 40 to 255
-	// Value: 150 to 255
-	// printf("checkExit");
+	
+	// If there is at least one object on channel 0...
 	if (get_object_count(0) > 0){
 		printf("Biggest Object Size: %d by %d\n", get_object_bbox(0,0).height, get_object_bbox(0,0).width);
 		printf("Number of Objects: %d\n", get_object_count(0));
+		
+		// Get the height and width of the largest object on that channel.
 		int h = get_object_bbox(0,0).height;
 		int w = get_object_bbox(0,0).width;
+		
+		// If the height is greater then 40 and the width is greater than 100, return true.
 		if (h > 40 && w > 100){
-			//camera_close();
 			pullUp();
 			return 1;
-		} else if (get_object_count(0) > 50){
+			
+			// Else if there are more than 50 objects detected on that channel, return true.
+			} else if (get_object_count(0) > 50){
 			pullUp();
 			return 1;
 		}
-		else {
-			//camera_close();
-			return 0;
-		}
-	} 
-	else {
-		//camera_close();
+		
+		// Otherwise return false.
+		else
 		return 0;
-	}
+	} 
+	// Otherwise return false.
+	else
+	return 0;
 }
 
-
+// MAIN
 int main(){ 
-	int lastTurn = 2;
-	int h = 0;
-	int w = 0;
+	int lastTurn = 2,		// Tracks the last turn made by PacMan, initially 2 (forward).
+	i;					// Used in for loop.
+	
+	// Setup the camera.
 	camera_open();
+	
+	// Loop until "break" is called.
 	while(1){
-		/*camera_update();
-		if (get_object_count(0) > 0){
-		h = get_object_bbox(0,0).height;
-		w = get_object_bbox(0,0).width;
-		printf("Height: %d, Width: %d\n",h,w);
-		}*/
-		int i;
-		for (i = 0; i < 1; i++){
-			lastTurn = solveMaze(lastTurn);
-		}
+		
+		// Run solveMaze three times.
+		for (i = 0; i < 1; i++)
+		lastTurn = solveMaze(lastTurn);
+		
+		// Reset trapped and counter.
 		reset();
+		
+		// Check for the exit, if found break from the loop.
 		if(checkExit()){
 			printf ("Exit Found!");
 			break;
 		}
 	}
+	
+	// Shutdown the camera.
 	camera_close();
 }	
